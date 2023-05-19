544f4837fdf32673f88bef9b7a83e2264084a392
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-05-20 04:27:16 +0600

    Update banner text and button

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -5,9 +5,9 @@ const Banner = () => {
         <div className='bg-[#c3915e] rounded-md relative h-screen'>
             <img className='mx-auto w-full h-full object-cover md:object-contain' src={banner} alt="" />
 
-            <div className='absolute space-y-8 md:max-w-sm text-center top-1/2  md:left-1/2 md:-translate-x-1/2 -translate-y-1/4'>
-                <p className='text-4xl font-semibold text-[#c3915e]'>Discover a world of cooking toys and tools that inspire creativity and learning.</p>
-                <button className='text-xl px-6 py-3 bg-primary font-semibold rounded-md'>Shop Now</button>
+            <div className='absolute space-y-7 md:max-w-sm text-center top-1/2  md:left-1/2 md:-translate-x-1/2 -translate-y-1/4'>
+                <p className='text-4xl font-bold text-[#c3915e] font-josefin'>Discover a world of cooking toys that inspire creativity and learning.</p>
+                <button className='text-xl px-8 py-3 bg-primary text-white font-semibold rounded-md'>Shop Now</button>
             </div>
         </div>
     );

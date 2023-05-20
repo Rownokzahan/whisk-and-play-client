658007bde95aa51c7cffcdf81b17b95031793b33
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-05-21 03:11:42 +0600

    Implement AOS animation

--- src/pages/Home/ChooseUs.jsx
+++ src/pages/Home/ChooseUs.jsx
@@ -1,16 +1,23 @@
 import { MdChildFriendly } from 'react-icons/md';
 import { VscWorkspaceTrusted } from 'react-icons/vsc';
 import { TbMoodBoy, TbHeartHandshake } from 'react-icons/tb';
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 
 const ChooseUs = () => {
+
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
     return (
         <div className="my-28">
             <h3 className="text-dark-gray font-josefin text-3xl text-center font-bold mb-12">Why Choose Us</h3>
 
             <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8 text-center">
-                <div className="border shadow rounded-md p-8 space-y-4">
+                <div className="border shadow rounded-md p-8 space-y-4" data-aos="zoom-in">
                     <div className='mx-auto w-max text-5xl text-purple-400'>
                         <TbMoodBoy />
                     </div>
@@ -18,21 +25,21 @@ const ChooseUs = () => {
                     <p className="text-gray-500">The toys are made from kid-friendly, non-toxic materials, ensuring a worry-free and enjoyable cooking experience.</p>
                 </div>
 
-                <div className="border shadow rounded-md p-8 space-y-4">
+                <div className="border shadow rounded-md p-8 space-y-4" data-aos="zoom-in">
                     <div className='w-max mx-auto text-5xl text-[#b6d6db]'>
                         <MdChildFriendly />
                     </div>
                     <h3 className="font-josefin text-2xl font-extrabold">Quality Products</h3>
                     <p className="text-gray-500">We prioritize child safety, offering age-appropriate toys that meet the highest safety standards.</p>
                 </div>
-                <div className="border shadow rounded-md p-8 space-y-4 relative">
+                <div className="border shadow rounded-md p-8 space-y-4 relative" data-aos="zoom-in">
                     <div className='w-max mx-auto text-5xl text-[#b16a4c]'>
                         <TbHeartHandshake />
                     </div>
                     <h3 className="font-josefin text-2xl font-extrabold">Trusted Brands</h3>
                     <p className="text-gray-500">We partner with reputable brands known for their quality and commitment to child development.</p>
                 </div>
-                <div className="border shadow rounded-md p-8 space-y-4">
+                <div className="border shadow rounded-md p-8 space-y-4" data-aos="zoom-in">
                     <div className='w-max mx-auto text-4xl text-green-300'>
                         <VscWorkspaceTrusted />
                     </div>

2dd3885b45771f6e8e9b77c067cab6c157960f71
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-05-21 17:43:36 +0600

    Display Toast notification for unauthorized users attempting to view toy details

--- src/components/cards/ToyCard.jsx
+++ src/components/cards/ToyCard.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
 import { Rating } from '@smastrom/react-rating';
 import '@smastrom/react-rating/style.css';
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import { toast } from "react-hot-toast";
 
 const ToyCard = ({ toy }) => {
 
+    const { user } = useContext(AuthContext);
     const { _id, imageURL, ToyName, ratings, price } = toy;
 
     return (
@@ -16,7 +20,21 @@ const ToyCard = ({ toy }) => {
                 </div>
                 <div className="flex justify-between items-center absolute bottom-4 left-2 right-2">
                     <p>${price}</p>
-                    <Link to={`toy-details/${_id}`} className="text-primary hover:underline duration-100 whitespace-nowrap">View Details</Link>
+                    <Link
+                        onClick={() => user ? "" :
+                            toast.error("You have to log in first to view details", {
+                                icon: '🙈',
+                                style: {
+                                    borderRadius: '4px',
+                                    background: '#333',
+                                    color: '#fff',
+                                    fontWeight: '600'
+                                },
+                            })}
+                        to={`toy-details/${_id}`}
+                        className="text-primary hover:underline duration-100 whitespace-nowrap"
+
+                    >View Details</Link>
                 </div>
             </div>
         </div>

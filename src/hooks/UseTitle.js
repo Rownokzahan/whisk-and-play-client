c023bcce7c7dc2d3ae6c67ebeef4a786ca6a7212
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-05-21 18:21:51 +0600

    Update useTitle hook to set document title as 'Whisk & Play | {title}'

--- src/hooks/UseTitle.js
+++ src/hooks/UseTitle.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 const useTitle = title => {
     useEffect(() => {
-        document.title = title + " | Whisk & Play"
+        document.title = "Whisk & Play | " + title;
     }, [title])
 }
 
